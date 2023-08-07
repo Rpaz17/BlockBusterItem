19df070ad3ac3dbeb62ccf39a7a1d65df246385d
fampa <fampa@RebecaLT>
2023-08-07 12:04:53 -0600

    MovieItem listo (solo falta fecha)

--- src/blockbusteritem/MovieItem.java
+++ src/blockbusteritem/MovieItem.java
@@ -1,8 +1,10 @@
 package blockbusteritem;
 
+import java.util.Calendar;
+
 public class MovieItem extends BlockBusterItem {
     public String estado;
-
+    
     public MovieItem(String estado, int codigo, String nombre, double precioRenta) {
         super(codigo, nombre, precioRenta);
         this.estado = "ESTRENO";
@@ -12,9 +14,28 @@ public class MovieItem extends BlockBusterItem {
     public String toString(){
           return  "Codigo: " + codigo +
                 ", Nombre: " + nombre + 
-                ", Precio de renta del Item: " + precioRenta + "}";
+                ", Precio de renta del Item: " + precioRenta +
+                  "Estado: "+ estado + 
+                  " - Movie";
     }
     
+    @Override
+    public double pagoRenta(int dias){
+        if(estado == "ESTRENO" && dias>2){
+            return precioRenta+(50*dias);
+        }else if (estado == "NORMAL" && dias>5){
+            return precioRenta+(30*(dias-5));
+        }else{
+            return precioRenta;
+        }
+    }
+    
+    public Calendar evaluarEstado(){
+//        Calendar fechaActual = Calendar.getInstance();
+//        if(fechaActual>)
+        return null;
+
+    }
     
     
 }
