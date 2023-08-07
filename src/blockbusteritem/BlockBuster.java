33e586c4ae0aa4b2e2154dabc1e4e4d2f0ef8814
pcast <pcast@DESK-E>
2023-08-07 11:37:44 -0600

    CONSTRUCTORES

--- src/blockbusteritem/BlockBuster.java
+++ src/blockbusteritem/BlockBuster.java
@@ -3,11 +3,33 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package blockbusteritem;
+import java.util.ArrayList;
 
-/**
- *
- * @author pcast
- */
+
 public class BlockBuster {
+    private ArrayList<BlockBusterItem> guardar;
+
+    public BlockBuster() {
+        guardar=new ArrayList();
+    }
     
 }
+/*
+Crear una clase llamada BlockBuster.
+1.	Tiene un atributo ARRAYLIST para guardar objetos MovieItem o VideoGameItem. En el constructor se inicializa.
+2.	Función buscarItem(int codigo, String tipo) busca en el arreglo un ítem con un código 
+y un tipo dado “MOVIE” o “GAME” dado. Si lo encuentra se retorna dicho objeto, si no, se retorna nulo.
+Se manda el tipo porque se permite una movie con código 1 al igual que un game con código 1.
+3.	Función agregarItem(int codigo, String nombre, String tipoItem).
+Se agrega un nuevo ítem a la colección según el tipoItem que puede ser “MOVIE” o “GAME” 
+solo si no existe ya un ítem con ese código y ese tipo. Cualquier otro dato extra se ingresa del teclado.
+4.	Rentar(int codigo, String tipoItem, int dias). 
+Si este ítem existe, se imprime todos los valores del objeto y se imprime su monto a pagar. 
+Si no existe se imprime “Item No Existe”.
+5.	auditarMovieEstados(). 
+Recorre toda la colección de ítems de manera recursiva y si el ítem es de movie, 
+se manda a llamar su función de evaluar el estado.
+
+
+
+*/
--- src/blockbusteritem/VideoGameItem.java
+++ src/blockbusteritem/VideoGameItem.java
@@ -5,10 +5,25 @@ package blockbusteritem;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
-/**
- *
- * @author pcast
- */
-public class VideoGameItem {
+public class VideoGameItem extends BlockBusterItem{
+    private String nameConsola;
+
+    public VideoGameItem(String nameConsola) {
+        super();
+        this.nameConsola = nameConsola;
+        renta=30;
+    }
+    
     
 }
+/*
+Crear una clase llamada VideoGameItem que hereda de la clase BlockBusterItem. 
+Además, tiene un atributo extra de tipo String para guardar el nombre de la consola. 
+En el constructor se pide de parámetro el tipo de consola, el precio de renta es fijo y es de Lps. 30.
+Tiene las siguientes funciones:
+1.	Se tiene 3 atributos estáticos finales de tipo String que guarda cada uno los nombres de las consolas válidas: 
+PLAYSTATION, XBOX y WII.
+2.	Redefine toString para adicionarle a lo que ya retorna el padre la consola más la palabra “Game”.
+3.	Redefine pagoRenta(int días) que retorna el precio de renta por cada día que se recibe.
+
+*/
